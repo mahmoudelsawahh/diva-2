7369ea2063202b71f50eb9781f3a8e80ef039fe0
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-09 05:12:34 +0300

    d

--- app/page.js
+++ app/page.js
@@ -1,47 +1,35 @@
 import dynamic from 'next/dynamic';
-const Loading = dynamic(() => import('./loading'),{
-  ssr : false,
-});
+
 const Distinguishes = dynamic(() => import('./components/Distinguishes/Distinguishes'),{
   ssr : false,
-  loading : ()=> <Loading/>
 });
 const MainSlider = dynamic(() => import('./components/MainSlider'),{
   ssr : false,
-  loading : ()=> <Loading/>
 });
 const AboutUs = dynamic(() => import('./components/about-homePage/AboutUs'),{
   ssr : false,
-  loading : ()=> <Loading/>
 });
 const Service = dynamic(() => import('./components/Service/Service'),{
   ssr : false,
-  loading : ()=> <Loading/>
 });
 const Goals = dynamic(() => import('./components/Goals/Goals'),{
   ssr : false,
-  loading : ()=> <Loading/>
 });
 const DevaStudioVideo = dynamic(() => import('./components/DevaStudioVideo/DevaStudioVideo'),{
   ssr : false,
-  loading : ()=> <Loading/>
 });
 const LatestNews = dynamic(() => import('./components/latestNews/LatestNews'),{
   ssr : false,
-  loading : ()=> <Loading/>
 });
 const InstagramBanner = dynamic(() => import('./components/InstagramBanner/InstagramBanner'),{
   ssr : false,
-  loading : ()=> <Loading/>
 });
 const Category = dynamic(() => import('./components/Category/Category'),{
   ssr : false,
-  loading : ()=> <Loading/>
 });
 
 const Footer = dynamic(() => import('./components/footer/Footer'),{
   ssr : false,
-  loading : ()=> <Loading/>
 });
 
 export const metadata = {
