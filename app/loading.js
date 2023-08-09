6524b294bee97e87e42dced685406cee9d82742a
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-09 05:25:50 +0300

    d

--- app/loading.js
+++ app/loading.js
@@ -1,14 +1,14 @@
 "use client"
 import { Box } from '@mui/material'
-import { InfinitySpin } from 'react-loader-spinner'
+// import { InfinitySpin } from 'react-loader-spinner'
 
 const Loading = () => {
   return (
     <div style={{width : '100%', height : '100vh', zIndex : 55555, display : 'flex', justifyContent : 'center', alignItems : 'center'}}>
-      <InfinitySpin 
+      {/* <InfinitySpin 
           width='200'
           color="#E60263"   
-             />   
+             />    */}
  </div>
   )
 }
