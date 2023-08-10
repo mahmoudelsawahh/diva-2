f57cdbc2697ed907d9f001e21e7c4fae352ddcd8
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-10 05:00:25 +0300

    d

--- app/components/MainSlider.jsx
+++ app/components/MainSlider.jsx
@@ -2,8 +2,8 @@
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 import Image from 'next/image'
-import firstBg from '/public/divanice.com_imgs_diva2.webp'
-import secondBg from '/public/divanice.com_imgs_diva3.webp'
+import firstBg from '/public/Artboard – 3'
+import secondBg from '/public/Artboard – 3'
 import { Typography } from '@/app/lib/MuiSsr'
 import dynamic from 'next/dynamic'
 const MainPage = dynamic(() => import('./MainPage'),{
