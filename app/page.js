5aaeb685f1f3daebf0bbb96fa11369af17d57e53
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-10 04:07:21 +0300

    d

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@
 import dynamic from 'next/dynamic';
 const MainSlider = dynamic(() => import('./components/MainSlider'),{
   ssr : false,
-  loading : ()=><h1>loading</h1>
 });
 
 const Footer = dynamic(() => import('./components/footer/Footer'),{
