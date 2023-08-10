d686ceffb1fc340f87611a238a89d68a62ccebcc
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-10 04:46:38 +0300

    d

--- app/components/blog-Page/SubBlog.jsx
+++ app/components/blog-Page/SubBlog.jsx
@@ -6,7 +6,7 @@ import { baseUrl } from '@/app/lib/baseUrl';
 const Loading = dynamic(() => import('@/app/loading'),{
   ssr : false,
 });
-import { Slide , Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, Grid} from '@/app/lib/MuiSsr';
+import { Slide , Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, Grid, Typography} from '@/app/lib/MuiSsr';
 import { Suspense } from 'react';
 import LazyLoad from 'react-lazyload';
 
@@ -27,12 +27,13 @@ const SubBlog = ({data}) => {
                      <CardMedia >
                          <Image src={`${baseUrl}/images?id=${item.imageId}`} width={250} height={200} alt={item.name} style={{ objectFit: 'cover', width : "100%" }}/>
                  </CardMedia>
-                 <CardContent sx={{height : '300px', overflow : 'hidden', margin : '5px 0px'}}>
-                  <div className='api-blog-data' dangerouslySetInnerHTML={{__html: item.description}}/>
+                 <CardContent sx={{minHeight : '80px', overflow : 'hidden', margin : '5px 0px'}}>
+                 <Typography variant='h1' sx={{fontSize : '22px', fontWeight : 'bold', textAlign : 'center'}}>{item.name}</Typography>
                  </CardContent>
                </CardActionArea>
                   <CardActions sx={{margin : '0px 10px'}}>
-                      <Button size="large" sx={{backgroundColor : '#555555', color : '#fff', fontWeight : 'bold', padding : '10px 25px', ":hover" : {backgroundColor : '#555555'}}}
+                      <Button size="large" sx={{backgroundColor : '#555555', color : '#fff',
+                       fontWeight : 'bold', padding : '10px 25px', ":hover" : {backgroundColor : '#555555'}}}
                       onClick={()=> router.push(`/blog/${item.id}/${item.name.split(' ').join('-')}`)}
                       >اقرأ المزيد</Button> 
                 </CardActions>
