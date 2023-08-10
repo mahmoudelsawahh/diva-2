abe46746d167e15ccf02d6ca8affc82db9673917
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-10 04:22:29 +0300

    d

--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
 import dynamic from 'next/dynamic';
 import MainPage from './components/MainPage';
 import MainSlider from './components/MainSlider';
-import Footer from './components/footer/Footer';
 // const MainSlider = dynamic(() => import('./components/MainSlider'),{
 //   ssr : false,
 // });
 
-// const Footer = dynamic(() => import('./components/footer/Footer'),{
-//   ssr : false,
-// });
+const Footer = dynamic(() => import('./components/footer/Footer'),{
+  ssr : false,
+});
 
 export const metadata = {
   title: "اتيليه فساتين زفاف وافراح",
