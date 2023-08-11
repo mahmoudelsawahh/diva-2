3101c5b9de309744eaf60f82820cf5bf58cd7ab6
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-11 22:45:09 +0300

    ss

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,39 +1,40 @@
 "use client"
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 // import LazyLoad from 'react-lazyload'
-const BlogTitle = dynamic(() => import('../components/blog-Page/BlogTitle'), {
-  ssr : false
-})
+// const BlogTitle = dynamic(() => import('../components/blog-Page/BlogTitle'), {
+//   ssr : false
+// })
 // const BlogContainer = dynamic(() => import('../components/blog-Page/BlogContainer'), {
 //   ssr : false
 // })
-const InstagramBanner = dynamic(() => import('../components/InstagramBanner/InstagramBanner'), {
-  ssr : false
-})
+// const InstagramBanner = dynamic(() => import('../components/InstagramBanner/InstagramBanner'), {
+//   ssr : false
+// })
 
-const Footer = dynamic(() => import('@/app/components/footer/Footer'),{
-  ssr : false
-});
+// const Footer = dynamic(() => import('@/app/components/footer/Footer'),{
+//   ssr : false
+// });
 
 export default  function Blog (){
   return (
    <>
-    <main>
+    {/* <main>
     <section>
       <BlogTitle/>
     </section>
-      {/* <LazyLoad height={"100%"} once>
+      <LazyLoad height={"100%"} once>
       <section>
              <BlogContainer/>
     </section>
-      </LazyLoad> */}
+      </LazyLoad>
     <section>
       <InstagramBanner/>
     </section>
     </main>
     <footer>
          <Footer/>
-      </footer>
+      </footer> */}
+      <h1>sssssssssssssssss</h1>
    </>
   )
 }
