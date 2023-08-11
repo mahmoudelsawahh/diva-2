a4782b75ee1ca23785e441062d25777f7b1a973d
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-11 17:23:45 +0300

    ss

--- app/components/InstagramBanner/InstagramBanner.jsx
+++ app/components/InstagramBanner/InstagramBanner.jsx
@@ -1,16 +1,16 @@
 "use client"
-import { Button, Container, Typography , Slide } from '@/app/lib/MuiSsr'
+import { Button, Container, Typography } from '@/app/lib/MuiSsr'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 const InstagramBanner = () => {
-  const router = useRouter()
   return (
      <Container maxWidth="xl" fixed>
-         <Slide right>
              <Typography variant='h1' sx={{fontSize : '2.5rem', fontWeight : 'bold', margin : '40px 0px', lineHeight : '90px'}}>تابع احدث الصور والعروض علي صفحتنا علي <br/> Instagram </Typography>
-         </Slide>
-        <Slide left>
-            <Button aria-label="صفحتنا عليInstagram" onClick={()=> router.push('https://www.instagram.com/diva_atelier100/')} variant='contained' sx={{backgroundColor : "#E60263", color : '#fff', fontWeight : 'bold', fontSize : '25px', ":hover" : {backgroundColor : '#E60263'}}}>الانتقال الي الصفحة</Button>
-        </Slide>
+           <Link href={'https://www.instagram.com/diva_atelier100'}>
+           <Button aria-label="صفحتنا عليInstagram" 
+           variant='contained' sx={{backgroundColor : "#E60263", color : '#fff', fontWeight : 'bold', fontSize : '25px', ":hover" : {backgroundColor : '#E60263'}}}>
+           الانتقال الي الصفحة</Button>
+           </Link>
     </Container>
 
   )
