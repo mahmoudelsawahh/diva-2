0b7c5b628474552e13d589a163384faa6219d32f
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-11 19:46:47 +0300

    ss

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,15 +1,8 @@
 import dynamic from 'next/dynamic'
  
-const Service = dynamic(() => import('../components/Service/Service'),{
+const Fake = dynamic(() => import('../components/FakeComponent/Fake'),{
   ssr : false
 })
-const InstagramBanner = dynamic(() => import('../components/InstagramBanner/InstagramBanner'),{
-  ssr : false
-})
-
-const Footer = dynamic(() => import('@/app/components/footer/Footer'),{
-  ssr : false
-});
 
 export const metadata = {
   title: "اتيليه فساتين زفاف وافراح",
@@ -17,17 +10,7 @@ export const metadata = {
 const About = () => {
   return (
     <>
-       <main>
-       <section>
-            <Service/>
-        </section>
-        <section>
-          <InstagramBanner/>
-        </section>
-       </main>
-       <footer>
-          <Footer/>
-       </footer>
+      <Fake/>
     </>
   )
 }
