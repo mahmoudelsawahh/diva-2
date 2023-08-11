1ccaf691eac320ed7558bb820f5754cacc126f0e
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-11 17:37:47 +0300

    ss

--- app/components/layoutContaner.js
+++ app/components/layoutContaner.js
@@ -5,13 +5,13 @@ import rtlPlugin from 'stylis-plugin-rtl';
 import dynamic from 'next/dynamic';
 import LazyLoad from 'react-lazyload';
 
-const DrawerAppBar = dynamic(() => import('@/app/components/NavBar'),{
-  ssr : false
-});
+// const DrawerAppBar = dynamic(() => import('@/app/components/NavBar'),{
+//   ssr : false
+// });
 
-const SocialFooter = dynamic(() => import('./footer/SocialFooter'),{
-  ssr : false
-});
+// const SocialFooter = dynamic(() => import('./footer/SocialFooter'),{
+//   ssr : false
+// });
 const cache = createCache({
   key: 'css',
   prepend: true,
