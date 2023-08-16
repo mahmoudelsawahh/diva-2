333dfddca64ccae234f92176f53dc6d0b2623091
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-16 15:08:55 +0300

    a

--- app/components/MainSlider.jsx
+++ app/components/MainSlider.jsx
@@ -19,9 +19,9 @@ const MainSlider = () => {
       <div className="embla__container">
           <div className="embla__slide">
           <div className="slide-content">
-                    <Typography variant='h1' sx={{fontSize : '15px', fontWeight : 'bold', textAlign : 'center'}}>أتيليه وميك أب أستوديو ديفا</Typography>
+                {/* <Typography variant='h1' sx={{fontSize : '15px', fontWeight : 'bold', textAlign : 'center'}}>أتيليه وميك أب أستوديو ديفا</Typography>
 			        <Typography sx={{fontSize : "2.3rem" , fontWeight : 'bold', marginBottom : '12px', textAlign : 'center'}}>أكبر أتيليه وميك أب ستوديو في المحلة الكبرى</Typography>
-			        <Typography variant='body1' sx={{fontSize : {xs : '20ox', md : '25px'}, textAlign : 'center'}}>مرحباً بكم في &quot; ديفا &quot; أتيليه وميك أب أستوديو</Typography>
+			        <Typography variant='body1' sx={{fontSize : {xs : '20ox', md : '25px'}, textAlign : 'center'}}>مرحباً بكم في &quot; ديفا &quot; أتيليه وميك أب أستوديو</Typography> */}
 		      	</div>
             <Image
               className="embla__slide__img"
@@ -33,7 +33,7 @@ const MainSlider = () => {
           </div>
           <div className="embla__slide">
           <div className="slide-content">  
-			         <Typography sx={{fontSize : "2.5rem" , fontWeight : 'bold', margin : '12px', textAlign : 'center'}}>نحن فخورون بتقديم خدماتنا للنساء في المحلة الكبرى بشكل خاص وجمهورية مصر العربية بشكل عام</Typography>
+			         {/* <Typography sx={{fontSize : "2.5rem" , fontWeight : 'bold', margin : '12px', textAlign : 'center'}}>نحن فخورون بتقديم خدماتنا للنساء في المحلة الكبرى بشكل خاص وجمهورية مصر العربية بشكل عام</Typography> */}
 	    		</div>
             <Image
               className="embla__slide__img"
